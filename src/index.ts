4969c784cbfa383a299824d6c301b294eecb8275
caisijie <caisijie@bytedance.com>
2020-11-06 17:38:00 +0800

    fix: typescript version

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,26 @@
-function getType(x: any) {
+function getType(x: any): string {
   return Object.prototype.toString.call(x).replace('object ', '');
 }
-const _updaters = {
-  initState({ action, getDefaultState }: { action: string, getDefaultState: () => Object }) {
-    return Object.assign({}, getDefaultState(), action.payload);
-  },
-  setState({ state, action }) {
-    return Object.assign({}, state, action.payload);
-  },
-  mergeState({ state, action }) {
-    const payload = action.payload
-    return Object.keys(payload).reduce(function (s, key) {
-      if (getType(s[key]) === '[Object]' && getType(payload[key]) === '[Object]') {
-        s[key] = Object.assign({}, s[key], payload[key])
-      } else {
-        s[key] = payload[key]
-      }
-      return s
-    }, Object.assign({}, state))
-  }
-}
+type Dictionary = { [key: string]: any }
+type PrimMeta<T> = { isPrimAction: boolean, namespace: string, updaterName: keyof PrimUpdaterImpls<T> };
+type PrimAction<T> = { type: string, payload: any, meta?: PrimMeta<T>, error?: any }
 
-var _actionTypePrefix = '@prim'
-export var extendUpdaters = function (namedUpdaters: { [name: string]: Function }) {
-  Object.assign(_updaters, namedUpdaters)
+interface UpdateParam<T extends Dictionary> {
+  state: T,
+  getDefaultState: Function,
+  action: PrimAction<T>
 }
+interface PrimUpdaterImpls<T extends Dictionary> {
+  initState: (p: UpdateParam<T>) => Dictionary,
+  setState: (p: UpdateParam<T>) => Dictionary,
+  mergeState: (p: UpdateParam<T>) => Dictionary
+}
+interface PrimUpdaters<T extends Dictionary> {
+  initState: (p: Dictionary) => PrimAction<T>,
+  setState: (p: Dictionary | ((s: T) => Dictionary)) => PrimAction<T>,
+  mergeState: (p: Dictionary) => PrimAction<T>
+}
+var _actionTypePrefix = '@prim'
 
 function stringify(x: any) {
   var type = getType(x);
@@ -34,10 +30,7 @@ function stringify(x: any) {
   return type
 }
 
-function querify(payload, intend) {
-  if (intend) {
-    return intend
-  }
+function querify(payload: Dictionary) {
   var payloadStr = stringify(payload)
   if (payloadStr === '[Object]') {
     return Object.keys(payload)
@@ -48,83 +41,87 @@ function querify(payload, intend) {
   return payloadStr
 }
 
-function updaterActionCreators(namespace) {
-  function getMeta(updaterName) {
-    var m = {
+function updaterActionCreators<T>(namespace: string): PrimUpdaters<T> {
+  function getMeta(updaterName: keyof PrimUpdaterImpls<T>): PrimMeta<T> {
+    return {
       isPrimAction: true,
       namespace,
-      signer
+      updaterName
     }
-    if (updaterName) {
-      m.updaterName = updaterName
-    }
-    return m
   }
   var ns = namespace
 
-  function updaterActionCreator(name) {
-    return function (payload, intend) {
+  function updaterActionCreator(updaterName: keyof PrimUpdaterImpls<T>) {
+    return function (payload: Dictionary | ((state: T) => Dictionary)): PrimAction<T> {
       return {
-        type: `${_actionTypePrefix}/${ns}/${name}/?${querify(payload, intend)}`,
+        type: `${_actionTypePrefix}/${ns}/${updaterName}/?${querify(payload)}`,
         payload,
-        meta: getMeta(name)
+        meta: getMeta(updaterName)
       }
     }
   }
 
   return {
-    ...Object.keys(_updaters).reduce(function (ret, updaterName) {
-      ret[updaterName] = updaterActionCreator(updaterName)
-      return ret
-    }, {}),
-    primAction(action) {
+    initState: updaterActionCreator('initState'),
+    setState: function (payload: Dictionary | ((state: T) => Dictionary)): PrimAction<T> {
       return {
-        type: `${_actionTypePrefix}/${ns}/${action.type}`,
-        payload: action,
-        meta: getMeta()
+        type: `${_actionTypePrefix}/${ns}/${'setState'}/?${querify(payload)}`,
+        payload,
+        meta: getMeta('setState')
       }
-    }
+    },
+    mergeState: updaterActionCreator('mergeState')
   }
 }
 
-export function createContractActions(namespace, creator) {
-  if (typeof creator !== 'function') {
-    throw new Error('Expected the creator to be a function.')
-  }
-  return function (signer, ...args) {
-    if (
-      typeof signer !== 'string' &&
-      typeof signer !== 'number' &&
-      typeof signer !== 'symbol'
-    ) {
-      throw new Error('Expected the signer to be a string , number or symbol.')
-    }
-    return Object.assign({}, creator(updaterActionCreators(namespace, signer), ...args), { signer })
-  }
-}
 
-export function createPrimActions(namespace: string, creator) {
-  if (typeof creator !== 'function') {
-    throw new Error('Expected the creator to be a function.')
-  }
-  return creator(updaterActionCreators(namespace))
-}
-
-function isMatchedAction(action, namespace) {
+function isMatchedAction<T>(action: PrimAction<T>, namespace: string) {
   var meta = action.meta
-  if (!meta || Object.prototype.toString(meta) !== '[object Object]') {
+  if (!meta || Object.prototype.toString.call(meta) !== '[object Object]') {
     return false
   }
   return meta.isPrimAction && meta.namespace === namespace
 }
 
+export function createPrimActions<T extends { [key: string]: (...args: any) => any }, P extends Dictionary>(
+  namespace: string,
+  getDefaultState: () => P,
+  creator: (updaters: PrimUpdaters<P>) => T
+) {
+  if (typeof creator !== 'function') {
+    throw new Error('Expected the creator to be a function.');
+  }
 
-export function createPrimReducer(namespace, getDefaultState, reducer) {
-  return function (state = getDefaultState(), action) {
+  const actions = creator(updaterActionCreators<P>(namespace));
+
+  const reducer = (state: P = getDefaultState(), action: PrimAction<P>): any => {
     if (!isMatchedAction(action, namespace)) return state
 
     var { updaterName } = action.meta
 
+    const _updaters: PrimUpdaterImpls<P> = {
+      initState({ action, getDefaultState }) {
+        return Object.assign({}, getDefaultState(), action.payload);
+      },
+      setState({ state, action }) {
+        if (typeof action.payload === 'function') {
+          let reducer = action.payload as ((s: T) => Dictionary);
+          return reducer(state);
+        }
+        return Object.assign({}, state, action.payload);
+      },
+      mergeState({ state, action }) {
+        const payload = action.payload
+        return Object.keys(payload).reduce(function (s, key: string) {
+          if (getType(s[key]) === '[Object]' && getType(payload[key]) === '[Object]') {
+            s[key] = Object.assign({}, s[key], payload[key])
+          } else {
+            s[key] = payload[key]
+          }
+          return s
+        }, Object.assign({}, state))
+      }
+    }
     if (updaterName) {
       return _updaters[updaterName]({
         state,
@@ -137,6 +134,23 @@ export function createPrimReducer(namespace, getDefaultState, reducer) {
         `reducer function is not defined in createPrimReducer("${namespace}", getDefaultState, reducer)`
       )
     }
-    return reducer(state, action.payload, getDefaultState)
   }
+  return { actions, reducer };
 }
+
+var todoActions = createPrimActions('todo',
+  () => ({
+    value1: '',
+    value2: 123,
+    value3: []
+  }),
+  ({ setState }) => {
+    return {
+      complexAction(data: Dictionary) {
+        return setState(state => ({ ...state }))
+      }
+    }
+  }
+);
+
+console.log(todoActions);
